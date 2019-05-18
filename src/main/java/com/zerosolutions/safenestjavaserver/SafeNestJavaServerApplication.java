7add700496862c09705a576fdc2d20097d00beec
Abhay Chandel <abhay1072@gmail.com>
2019-05-18 14:15:44 +0530

    Building Safe Nests :)

---
+++ src/main/java/com/zerosolutions/safenestjavaserver/SafeNestJavaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zerosolutions.safenestjavaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeNestJavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeNestJavaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerosolutions/safenestjavaserver/SafeNestJavaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zerosolutions.safenestjavaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SafeNestJavaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
